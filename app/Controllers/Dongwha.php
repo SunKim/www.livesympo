06f91deb661b1f8ecae3b3d2f1b348fb8d84c867
SunKim <sjmarine97@gmail.com>
2021-05-21 16:53:35 +0900

    동화약품 202105 반영 ing

--- app/Controllers/Dongwha.php
+++ app/Controllers/Dongwha.php
@@ -9,6 +9,24 @@ class Dongwha extends BaseController {
   	}
 
 	public function index () {
+		return $this->dw202105();
+	}
+
+	public function dw202105 () {
+		$data['DONGWHA_202105_LEC1_READY_YN'] = $this->settingModel->value('DONGWHA_202105_LEC1_READY_YN');
+		$data['DONGWHA_202105_LEC2_READY_YN'] = $this->settingModel->value('DONGWHA_202105_LEC2_READY_YN');
+		$data['DONGWHA_202105_LEC3_READY_YN'] = $this->settingModel->value('DONGWHA_202105_LEC3_READY_YN');
+		$data['DONGWHA_202105_LEC4_READY_YN'] = $this->settingModel->value('DONGWHA_202105_LEC4_READY_YN');
+
+		$data['DONGWHA_202105_LEC1_FILE_NM'] = $this->settingModel->value('DONGWHA_202105_LEC1_FILE_NM');
+		$data['DONGWHA_202105_LEC2_FILE_NM'] = $this->settingModel->value('DONGWHA_202105_LEC2_FILE_NM');
+		$data['DONGWHA_202105_LEC3_FILE_NM'] = $this->settingModel->value('DONGWHA_202105_LEC3_FILE_NM');
+		$data['DONGWHA_202105_LEC4_FILE_NM'] = $this->settingModel->value('DONGWHA_202105_LEC4_FILE_NM');
+
+		return view('dongwha/dongwha_202105.php', $data);
+	}
+
+	public function dw202012 () {
 		$data['DONGWHA_202012_LEC1_READY_YN'] = $this->settingModel->value('DONGWHA_202012_LEC1_READY_YN');
 		$data['DONGWHA_202012_LEC2_READY_YN'] = $this->settingModel->value('DONGWHA_202012_LEC2_READY_YN');
 		$data['DONGWHA_202012_LEC3_READY_YN'] = $this->settingModel->value('DONGWHA_202012_LEC3_READY_YN');
